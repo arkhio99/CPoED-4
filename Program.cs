962189669fd9550cd469f04f0c8e83a09130d2a1
arkhio99 <vladbashev5@gmail.com>
2020-05-10 18:05:24 +0500

    Added Mathematical Expectation and Dispersion

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CPoED_4
     class Program
     {
         delegate Tout myFunc<Tin,Tout>(Tin x);
-        static Tout[] Mapper<Tin,Tout>(Tin[] ar, myFunc<Tin,Tout> f)
+        static Tout[] Mapper<Tin,Tout>(Tin[] ar, myFunc<Tin,Tout> f)//Функция для маппинга
         {
             int n=ar.Length;
             Tout[] res=new Tout[n];
@@ -14,16 +14,48 @@ namespace CPoED_4
                 res[i]=f(ar[i]);
             return res;
         }
-        static string path=@"C:\Users\vladb\Desktop\Input.txt";
+        static string pathInput=@"C:\Users\vladb\Desktop\Input.txt";
+        static string pathOutput="out.txt";
         static double[] FileIntoArray(string path)
         {
             string s=File.ReadAllText(path);
             double[] res=Mapper(s.Split('\n'),t=>Convert.ToDouble(t));
             return res;
         }
+
+        static double GetMathExpect(double[] ar)//Получение мат. ожидания
+        {
+            double res=0;
+            double n=ar.Length;
+            for(int i=0;i<n;i++)
+            {
+                res+=ar[i];
+            }
+            return res/n;
+        }
+        static double GetDispersion(double[] ar, double MathExpect)//Получение дисперсии
+        {
+            double res=0;
+            double n=ar.Length;
+            for(int i=0;i<n;i++)
+                res+=(ar[i]-MathExpect)*(ar[i]-MathExpect);
+            return res/n;
+        }
         static void Main(string[] args)
         {
+            using (var output = new StreamWriter(pathOutput))
+            {
+            double[] data=FileIntoArray(pathInput);
+            int n=data.Length;
+            for(int i=0;i<n;i++)
+                output.WriteLine($"{i+1} неделя: {data[i]}");
+            double mathExpect=GetMathExpect(data);
+            output.WriteLine($"Математическое ожидание: {mathExpect}");
+            double dispersion=GetDispersion(data,mathExpect);
+            output.WriteLine($"Дисперсия: {dispersion}");
+
             
+            }
         }
     }
 }
