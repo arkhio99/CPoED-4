1383cc6e85511d7dcbc679b6d84e24f54d12574a
arkhio99 <vladbashev5@gmail.com>
2020-05-10 19:21:36 +0500

    Second task done

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ namespace CPoED_4
 {
     class Program
     {
-        delegate Tout myFunc<Tin,Tout>(Tin x);
-        static Tout[] Mapper<Tin,Tout>(Tin[] ar, myFunc<Tin,Tout> f)//Функция для маппинга
+        //delegate Tout myFunc<Tin,Tout>(Tin x);
+        static Tout[] Mapper<Tin,Tout>(Tin[] ar, Func<Tin,Tout> f)//Функция для маппинга
         {
             int n=ar.Length;
             Tout[] res=new Tout[n];
@@ -41,6 +41,10 @@ namespace CPoED_4
                 res+=(ar[i]-MathExpect)*(ar[i]-MathExpect);
             return res/n;
         }
+        static double GeS1By_t(double alpha, double x,double s1Last)
+        {
+            return alpha*x+(1-alpha)*s1Last;
+        }
         static void Main(string[] args)
         {
             using (var output = new StreamWriter(pathOutput))
@@ -55,7 +59,32 @@ namespace CPoED_4
             double dispersion=GetDispersion(data,mathExpect);
             output.WriteLine($"Дисперсия: {dispersion}");
 
+            output.WriteLine("\n\n2 пункт:");
+            double alpha =0.1;
+
+            double[] s1=new double[n];
+            for(int i=0;i<5;i++)
+                s1[0]+=data[i];
+            s1[0]/=5;
+            for(int i=1;i<n;i++)
+            {
+                s1[i]=GeS1By_t(alpha,data[i],s1[i-1]);
+            }
+            output.WriteLine("Предсказанные значения:");
+            for(int i=0;i<n;i++)
+                output.WriteLine($"{i+1} неделя: {s1[i]}");
+
+            double[] errorsX_1=new double[n];
+            for(int i=0;i<n;i++)
+                errorsX_1[i]=s1[i]-data[i];
+            output.WriteLine("\n\nОшибки:");
+            for(int i=0;i<n;i++)
+                output.WriteLine($"{i+1} неделя: {errorsX_1[i]}");
+
+            double dispersionOfErrors_1=GetDispersion(errorsX_1,GetMathExpect(errorsX_1));
+            output.WriteLine($"Дисперсия: {dispersionOfErrors_1}");
             
+
             }
         }
     }
